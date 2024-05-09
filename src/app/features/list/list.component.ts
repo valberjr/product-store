b21c52d32a2196f794e3a403d71b9c7363e837b5
valberjr <valber@duck.com>
2024-05-09 16:27:43 -0300

    add cards to display products

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { Product } from '../../shared/interfaces/product.interface';
 import { ProductsService } from '../../shared/services/products.service';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
