6c326d0a177c122f04049feac2dfbe9193d6d517
valberjr <valber@duck.com>
2024-05-09 16:43:36 -0300

    refactoring the product list by creating a component to render it

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,13 +1,12 @@
 import { Component, inject } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 import { Product } from '../../shared/interfaces/product.interface';
 import { ProductsService } from '../../shared/services/products.service';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [CardComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
