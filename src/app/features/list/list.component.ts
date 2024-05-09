d7e18845827c8f5dfb970b5af3e51299ae4035c4
valberjr <valber@duck.com>
2024-05-09 11:52:13 -0300

    refactor product listing

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+import { Product } from '../../shared/interfaces/product.interface';
+import { ProductsService } from '../../shared/services/products.service';
 
 @Component({
   selector: 'app-list',
@@ -9,12 +10,12 @@ import { Component, inject } from '@angular/core';
   styleUrl: './list.component.scss',
 })
 export class ListComponent {
-  products: any[] = [];
+  products: Product[] = [];
 
-  httpClient = inject(HttpClient);
+  productService = inject(ProductsService);
 
   constructor() {
-    this.httpClient.get<any>('/api/products').subscribe((products) => {
+    this.productService.getAll().subscribe((products) => {
       this.products = products;
     });
   }
