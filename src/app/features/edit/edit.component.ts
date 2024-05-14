22e2553e393e5fddfd9c0a9d136ddd70a9f1af50
valberjr <valber@duck.com>
2024-05-14 14:20:20 -0300

    feat: use the back-to-list component in both edit and create forms

--- src/app/features/edit/edit.component.ts
+++ src/app/features/edit/edit.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
+import { BackToListComponent } from '../../shared/components/back-to-list/back-to-list.component';
 import { FormComponent } from '../../shared/components/form/form.component';
 import { Product } from '../../shared/interfaces/product.interface';
 import { ProductsService } from '../../shared/services/products.service';
@@ -9,7 +10,7 @@ import { ProductsService } from '../../shared/services/products.service';
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [FormComponent],
+  imports: [FormComponent, BackToListComponent],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss',
 })
